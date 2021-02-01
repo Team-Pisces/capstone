4306775e876fec2208167411165110b5d2e7f403
Sean Murphy <scmurphy96@gmail.com>
2021-02-01 15:06:41 -0500

    feat(added transactions component)

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -13,6 +13,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
           <Link to="/habits">Habits</Link>
+          <Link to="/transactions">Transactions</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
