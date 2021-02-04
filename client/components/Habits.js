49abe0b9d2afb5213055717aea913acc69d181c4
Ned Brennan <nedbrnnn@gmail.com>
2021-02-04 12:22:53 -0500

    feat(reformats habit form to reflect new design pattern)

--- client/components/Habits.js
+++ client/components/Habits.js
@@ -2,13 +2,27 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchHabits, addHabit} from '../store/habits'
 import {fetchCategories} from '../store/categories'
+import {
+  Box,
+  Typography,
+  FormGroup,
+  FormContent,
+  InputLabel,
+  Input,
+  Button,
+  Select,
+  MenuItem,
+  FormHelperText,
+  FormControl,
+  Card,
+  CardContent
+} from '@material-ui/core'
 
 class Habits extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       name: '',
-      category: '',
       goal: 0
     }
 
@@ -27,71 +41,63 @@ class Habits extends React.Component {
     this.props.addHabit(this.state)
   }
 
-  componentDidMount() {
-    this.props.fetchHabits()
-    this.props.fetchCategories()
-  }
-
   render() {
     const habits = this.props.habits || []
     const {categories} = this.props
     return (
-      <div>
-        <h1>Habits</h1>
-        <div>
-          {habits.length ? (
-            habits.map((habit, i) => {
-              return (
-                <div key={i}>
-                  <h3>
-                    Habit: {habit.name} | Goal: ${habit.goal}
-                  </h3>
-                </div>
-              )
-            })
-          ) : (
-            <h1>No Habits</h1>
-          )}
-        </div>
-        <div>
-          <form id="add-habit-form" onSubmit={this.handleSubmit}>
-            <label htmlFor="habit">Habit: </label>
-            <input
-              name="name"
-              type="text"
-              onChange={this.handleChange}
-              value={this.state.name}
-            />
+      <Box display="flex" justifyContent="center">
+        <Box width="25vw" paddingTop="40px" paddingRight="20px">
+          <Card>
+            <CardContent>
+              <Typography>Habit Name</Typography>
+            </CardContent>
+          </Card>
+        </Box>
+        <Box width="25vw" paddingTop="40px" paddingLeft="20px">
+          <Typography variant="h5">New Habit</Typography>
+          <FormGroup id="add-habit-form" onSubmit={this.handleSubmit}>
+            <FormControl>
+              <InputLabel htmlFor="habit">Habit: </InputLabel>
+              <Input
+                name="name"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.name}
+              />
+            </FormControl>
 
-            <label htmlFor="category">Category: </label>
-            <select
-              name="category"
-              onChange={this.handleChange}
-              value={this.state.category}
-            >
-              <option>Select Category</option>
-              {categories.length
-                ? categories.map(category => {
-                    return (
-                      <option key={category.category_id}>
-                        {category.hierarchy[category.hierarchy.length - 1]}
-                      </option>
-                    )
-                  })
-                : null}
-            </select>
-
-            <label htmlFor="goal">Goal: </label>
-            <input
-              name="goal"
-              type="number"
-              onChange={this.handleChange}
-              value={this.state.goal}
-            />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      </div>
+            {/* <label htmlFor="category">Category: </label>
+              <Select
+                name="category"
+                onChange={this.handleChange}
+                value={this.state.category}
+              >
+                <option>Select Category</option>
+                {categories.length
+                  ? categories.map((category) => {
+                      return (
+                        <MenuItem key={category.category_id}>
+                          {category.hierarchy[category.hierarchy.length - 1]}
+                        </MenuItem>
+                      )
+                    })
+                  : null}
+              </Select> */}
+            <FormControl>
+              <FormHelperText htmlFor="goal">Goal: </FormHelperText>
+              <Input
+                name="goal"
+                type="number"
+                onChange={this.handleChange}
+                value={this.state.goal}
+              />
+              <Button variant="contained" color="primary" type="submit">
+                Submit
+              </Button>
+            </FormControl>
+          </FormGroup>
+        </Box>
+      </Box>
     )
   }
 }
--- server/api/habits.js
+++ server/api/habits.js
@@ -5,11 +5,7 @@ module.exports = router
 // GET api/habits
 router.get('/', async (req, res, next) => {
   try {
-    const habits = await Habit.findAll({
-      where: {
-        userId: req.user.id
-      }
-    })
+    const habits = await Habit.findAll()
     res.send(habits)
   } catch (err) {
     next(err)
@@ -21,7 +17,6 @@ router.post('/', async (req, res, next) => {
   try {
     const habit = await Habit.create({
       name: req.body.name,
-      categories: [...[req.body.category]],
       goal: req.body.goal,
       userId: req.user.id
     })
