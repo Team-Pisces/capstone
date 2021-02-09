1f8f491d7f5a80e3bf64d40c322ddde11fba8405
Ned Brennan <nedbrnnn@gmail.com>
2021-02-09 18:04:51 -0500

    fix(redChart): solves rendering issue

--- client/components/RedChart.js
+++ client/components/RedChart.js
@@ -6,19 +6,20 @@ import compound from 'compound-interest-calc'
 
 class RedChart extends React.Component {
   compoundInterest = (weeklyAvg, year) => {
-    console.log(compound(0, weeklyAvg, year, 0.07, 52))
     return compound(0, weeklyAvg, year, 0.07, 52).result
   }
 
   render() {
     const compoundInterest = this.compoundInterest
     const weeklyAvg = this.props.weeklyAvg || null
-    let years = Array(31)
-      .fill(0, 0, 31)
+    let years = Array(30)
+      .fill(1, 0, 30)
       .map((num, index) => num + index)
 
     let display = years.filter(num => num % 2 === 0)
     let displayText = display.map(val => val.toString())
+
+    console.log(compoundInterest(weeklyAvg, years[0]))
     return (
       <div>
         {this.props.weeklyAvg ? (
@@ -29,6 +30,7 @@ class RedChart extends React.Component {
                 <VictoryTooltip cornerRadius={0} style={{fill: 'red'}} />
               }
               data={display.map(year => {
+                console.log(compoundInterest(weeklyAvg, year))
                 return {
                   x: year,
                   y: compoundInterest(weeklyAvg, year),
